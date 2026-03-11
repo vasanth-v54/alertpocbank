8ab7c2f7cdc51007e97e29eea96a5dd5ea16b9ae
Vasanth <ajai.vasanth.work@gmail.com>
2026-03-12 00:16:17 +0530

    Deleted Old Approach

--- Consumer_Setup/src/main/java/com/poc/alerts/repository/ProcessedAlertAuditRepository.java
+++
@@ -1,8 +0,0 @@
-package com.poc.alerts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.poc.alerts.entity.ProcessedAlertAudit;
-
-public interface ProcessedAlertAuditRepository extends JpaRepository<ProcessedAlertAudit, Long> {
-	boolean existsByEventIdAndMessageType(String eventId, String messageType);
-}
