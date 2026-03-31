49fe1ea8b0ab462c0a735bbf82bb5a2459ecda77
Vasanth <ajai.vasanth.work@gmail.com>
2026-03-31 13:14:30 +0530

    Old Consumer Setup Removed

--- Consumer_Setup/src/main/java/com/poc/alerts/repository/AuditRepository.java
+++
@@ -1,8 +0,0 @@
-package com.poc.alerts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.poc.alerts.entity.AuditMst;
-
-public interface AuditRepository extends JpaRepository<AuditMst, Long> {
-	boolean existsByEventIdAndMessageType(String eventId, String messageType);
-}
