62a5ccd680ecc3fda4647b4a0f4ad1f38729f257
shubhmp98 <shubham.gupta@mindmaptechnologies.net>
2026-04-08 15:25:24 +0530

    Update template_master and dxp_email_template, dxp_sms_template table

--- TemplateManager/src/main/resources/Scripts/Table_Scripts.sql
+++ TemplateManager/src/main/resources/Scripts/Table_Scripts.sql
@@ -2,47 +2,48 @@ CREATE DATABASE IF NOT EXISTS DXP;
 
 USE DXP;
 
-CREATE TABLE DXP_SMS_TEMPLATE (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    TEMPLATE_NAME VARCHAR(255) NOT NULL UNIQUE,
-    TEMPLATE_BODY TEXT NOT NULL,
-    IS_ACTIVE TINYINT(1) NOT NULL DEFAULT 1,
-    DATE_CREATED DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    DATE_UPDATED DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE dxp.DXP_SMS_TEMPLATE (
+                                      ID INT AUTO_INCREMENT PRIMARY KEY,
+                                      TEMPLATE_NAME VARCHAR(255) NOT NULL UNIQUE,
+                                      TEMPLATE_BODY JSON NOT NULL,
+                                      IS_ACTIVE BOOLEAN NOT NULL DEFAULT TRUE,
+                                      DATE_CREATED DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                      DATE_UPDATED DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
 );
-CREATE TABLE DXP_EMAIL_TEMPLATE (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    TEMPLATE_NAME VARCHAR(255) NOT NULL UNIQUE,
-    TEMPLATE_BODY TEXT NOT NULL,
-    IS_ACTIVE TINYINT(1) NOT NULL DEFAULT 1,
-    DATE_CREATED DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    DATE_UPDATED DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
+
+CREATE TABLE dxp.DXP_EMAIL_TEMPLATE (
+                                        ID INT AUTO_INCREMENT PRIMARY KEY,
+                                        TEMPLATE_NAME VARCHAR(255) NOT NULL UNIQUE,
+                                        TEMPLATE_BODY JSON NOT NULL,
+                                        IS_ACTIVE BOOLEAN NOT NULL DEFAULT TRUE,
+                                        DATE_CREATED DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                        DATE_UPDATED DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
 -----Updated template_master table
 
 CREATE TABLE dxp.template_master (
-                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                     template_name VARCHAR(255) NOT NULL,
-                                     version VARCHAR(10),
-                                     message_type VARCHAR(20),
-                                     alert_config JSON,
-                                     headers JSON,
-                                     raw_content JSON,
-                                     indexed_content JSON,
-                                     param_mapping JSON,
-                                     contentHash VARCHAR(255),
-                                     exceptionReason VARCHAR(255),
-                                     isDuplicateallowed BOOLEAN DEFAULT FALSE,
-                                     is_active VARCHAR(1) DEFAULT '1',
-                                     created_by VARCHAR(50),
-                                     modified_by VARCHAR(50),
-                                     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                     modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                                     created_at TIMESTAMP NULL,
-                                     updated_at TIMESTAMP NULL,
-                                     updated_by VARCHAR(50),
-                                     CONSTRAINT uk_template UNIQUE (template_name, is_active)
+                                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                         template_name VARCHAR(255) NOT NULL,
+                                         version VARCHAR(20),
+                                         message_type VARCHAR(20),
+                                         alert_config JSON,
+                                         headers JSON,
+                                         raw_content JSON,
+                                         indexed_content JSON,
+                                         param_mapping JSON,
+                                         contentHash VARCHAR(255),
+                                         exceptionReason VARCHAR(255),
+                                         isDuplicateallowed BOOLEAN DEFAULT FALSE,
+                                         is_active VARCHAR(1) DEFAULT '1',
+                                         created_by VARCHAR(50),
+                                         modified_by VARCHAR(50),
+                                         created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                         modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                         created_at TIMESTAMP NULL,
+                                         updated_at TIMESTAMP NULL,
+                                         updated_by VARCHAR(50),
+                                         CONSTRAINT uk_template UNIQUE (template_name, message_type, is_active)
 );
 
 
