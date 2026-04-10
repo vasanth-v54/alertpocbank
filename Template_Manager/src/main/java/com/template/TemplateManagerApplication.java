3e51f2d6e51ae25b3c39a412507876fb0aeafb74
shubhmp98 <shubham.gupta@mindmaptechnologies.net>
2026-04-10 10:59:50 +0530

    Main class TemplateManagerApplication update

--- Template_Manager/src/main/java/com/template/TemplateManagerApplication.java
+++ Template_Manager/src/main/java/com/template/TemplateManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiDevlopApplication {
+public class TemplateManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiDevlopApplication.class, args);
+		SpringApplication.run(TemplateManagerApplication.class, args);
 	}
 
 }
